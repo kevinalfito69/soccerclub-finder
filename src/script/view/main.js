bed0fe1e0b10bf5273c0d285d07da32db2ed5794
kevinalfito69 <kevinalfito69@gmail.com>
2022-08-30 22:47:39 +0700

    Add custom element

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,8 +1,10 @@
+import '../component/search-bar.js';
+import '../component/club-list.js';
+
 import DataSource from '../data/data-source.js';
 const main = () => {
-    const searchElement = document.querySelector('#searchElement');
-    const buttonSearchElement = document.querySelector('#searchButtonElement');
-    const clubListElement = document.querySelector('#clubList');
+    const searchElement = document.querySelector('search-bar');
+    const clubListElement = document.querySelector('club-list');
 
     const onButtonSearchClicked = async () => {
         const result = await DataSource.searchClub(searchElement.value);
@@ -14,27 +16,13 @@ const main = () => {
     };
 
     const renderResult = (results) => {
-        clubListElement.innerHTML = '';
-        results.forEach(function (club) {
-            const { name, fanArt, description } = club;
-            const clubElement = document.createElement('div');
-            clubElement.setAttribute('class', 'club');
-
-            clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-          <div class="club-info">
-          <h2>  ${name}  </h2>
-          <p>  ${description}  </p> 
-
-          </div>`;
-            clubListElement.appendChild(clubElement);
-        });
+        clubListElement.clubs = results;
     };
 
     const fallbackResult = (message) => {
-        clubListElement.innerHTML = '';
-        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+        clubListElement.renderError(message);
     };
 
-    buttonSearchElement.addEventListener('click', onButtonSearchClicked);
+    searchElement.clickEvent = onButtonSearchClicked;
 };
 export default main;

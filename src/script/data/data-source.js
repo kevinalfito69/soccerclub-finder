76acdfed7b9bc588b998dbb114e32632c0612e73
kevinalfito69 <kevinalfito69@gmail.com>
2022-09-17 20:41:33 +0700

    Update data using API

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -2,17 +2,19 @@ import clubs from "./clubs.js";
 
 class DataSource {
     static searchClub(keyword) {
-        return new Promise((resolve, reject) => {
-            const filteredClubs = clubs.filter((club) =>
-                club.name.toUpperCase().includes(keyword.toUpperCase())
-            );
-
-            if (filteredClubs.length) {
-                resolve(filteredClubs);
-            } else {
-                reject(`${keyword} is not found`);
-            }
-        });
+        return fetch(
+            `https://sports-api.dicoding.dev/teams/search?t=${keyword}`
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((responseJSON) => {
+                if (responseJSON.teams) {
+                    return Promise.resolve(responseJSON.teams);
+                } else {
+                    return Promise.reject(`${keyword}Not found`);
+                }
+            });
     }
 }
 
